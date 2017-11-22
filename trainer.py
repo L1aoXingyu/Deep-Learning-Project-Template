b6fb3c175301dac4978c8791f0c2938a2486b461
SherlockLiao <sherlockliao01@gmail.com>
2017-11-22 21:00:15 +0800

    update shcedule optimizer

---
+++ trainer.py
@@ -0,0 +1,25 @@
+class ScheduledOptim(object):
+    '''A wrapper class for learning rate scheduling'''
+
+    def __init__(self, optimizer):
+        self.optimizer = optimizer
+        self.lr = self.optimizer.param_groups[0]['lr']
+        self.current_steps = 0
+
+    def step(self):
+        "Step by the inner optimizer"
+        self.current_steps += 1
+        self.optimizer.step()
+
+    def zero_grad(self):
+        "Zero out the gradients by the inner optimizer"
+        self.optimizer.zero_grad()
+
+    def set_learning_rate(self, lr):
+        self.lr = lr
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = lr
+
+    @property
+    def learning_rate(self):
+        return self.lr
